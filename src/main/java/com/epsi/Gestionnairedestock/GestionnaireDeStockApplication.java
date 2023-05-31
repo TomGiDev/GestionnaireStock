c5fc0e437648daea5cd31ec4747e56c60d796ce5
TomGiDev <thomas.girardet@orange.fr>
2023-05-31 11:30:39 +0200

    first commit

---
+++ src/main/java/com/epsi/Gestionnairedestock/GestionnaireDeStockApplication.java
@@ -0,0 +1,14 @@
+package com.epsi.Gestionnairedestock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionnaireDeStockApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(GestionnaireDeStockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epsi/Gestionnairedestock/GestionnaireDeStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epsi.Gestionnairedestock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionnaireDeStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
